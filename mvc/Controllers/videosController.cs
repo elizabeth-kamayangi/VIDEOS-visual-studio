33004ca4218c92449e316fda54d69b3d4c2e9920
kamayangi elizabeth <kamayangie@gmail.com>
2017-07-18 12:44:06 +0300

    fourth commit

---
+++ mvc/Controllers/videosController.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using mvc.Models;
+
+namespace mvc.Controllers
+{
+  public class videosController : Controller
+  {
+    private ApplicationDbContext _dbContext;
+
+    public videosController()
+    {
+        _dbContext = new ApplicationDbContext();
+        
+    }
+    public ActionResult Index()
+        {
+        var videos = _dbContext.Videos.ToList();
+        return View(videos);
+        }
+    }
+}
